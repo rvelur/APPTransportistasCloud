c1919a5262d408d61a4fcdecd8bede4064fc8d15
Roberto Velasquez <rvelasquez579@gmail.com>
2026-06-27 04:26:20 -0400

    Se cambia estrategia de ID a AUTO para compatibilidad con Oracle

--- src/main/java/com/duoc/transportistas/model/GuiaDespacho.java
+++ src/main/java/com/duoc/transportistas/model/GuiaDespacho.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class GuiaDespacho {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false, unique = true)
